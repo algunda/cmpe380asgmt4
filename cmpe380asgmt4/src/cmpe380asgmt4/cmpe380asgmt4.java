aade331f88620734207f1a6cd209cbd7f5a5894c
Sam Codrington <sam.codrington@queensu.ca>
2016-11-14 19:40:24 -0500

    formatted, commented project is finished

--- cmpe380asgmt4/src/cmpe380asgmt4/cmpe380asgmt4.java
+++ cmpe380asgmt4/src/cmpe380asgmt4/cmpe380asgmt4.java
@@ -8,16 +8,13 @@ public class cmpe380asgmt4 {
 
 	public static void main(String[] args) {
 		scheduler partA=new scheduler("Qooqle_Projects_8_6_1477962811.78.txt");
-		//partA.printMe();
-		//String amsg=partA.genResult();
-
 		scheduler partB=new scheduler("Qooqle_Projects_10_30_1477962181.44.txt");
 		scheduler partC=new scheduler("Qooqle_Projects_52_150_1477962917.86.txt"); 
 		scheduler partD=new scheduler("Qooqle_Projects_500_1500_1477962966.94.txt"); 
 		try{
-			PrintWriter w = new PrintWriter(new File("Answers.txt"));
-
-			w.write("File 1" + partA.genResult());
+			PrintWriter w = new PrintWriter(new File("Output.txt"));
+			w.write("Answers to Lab 4 by Sam Codrington & Aidan Gunda");
+			w.write("\nFile 1\n" + partA.genResult());
 			w.write("\nFile 2\n" + partB.genResult());
 			w.write("\nFile 3\n" + partC.genResult());
 			w.write("\nFile 4\n" + partD.genResult());
--- cmpe380asgmt4/src/cmpe380asgmt4/scheduler.java
+++ cmpe380asgmt4/src/cmpe380asgmt4/scheduler.java
@@ -7,17 +7,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class scheduler {
+
 	private String file;
 	private int numweeks, numproj;
 	private int[][] filledTable;
 	ArrayList<project> projList=new ArrayList<project>();
+
 	public scheduler(String filename){
 		file=filename;
 		readFile();
 		dynamic();
 	}
-	private void readFile(){
 
+	private void readFile(){
 		try {
 			Scanner sc = new Scanner(new File(file));
 			numweeks= sc.nextInt();
@@ -36,7 +38,7 @@ public class scheduler {
 			ex.printStackTrace();
 		} 
 	}
-	
+
 	private void dynamic(){
 		int[][] val=new int [numweeks] [numproj];
 		for (int x=0; x<numweeks; x++) {
@@ -49,7 +51,8 @@ public class scheduler {
 					 * the value of the current project plus the value from before the start of 
 					 * the current project.
 					 */
-					int pot;
+
+					int pot;							//setting potential
 					if (proj.start==1) 
 						pot=proj.val; //if it starts in week 1, the value before will be 0
 					else 
@@ -58,13 +61,14 @@ public class scheduler {
 					 *that will be the maximum of the week. Otherwise a project with a higher number than the current one
 					 *that finished on that week won't be considered
 					 */
-					int prev;
+
+					int prev;							//setting previous
 					if (y==0)							//if it's the first project
 						prev=val[x-1][numproj-1];		//check the last filled value of the last project
 					else	
 						prev=val[x][y-1];
 
-					val[x][y]=max(pot,prev);
+					val[x][y]=max(pot,prev);			//take maximum of the two
 				}
 
 				else {
@@ -82,6 +86,7 @@ public class scheduler {
 	}
 
 	public String genResult(){
+		//Find optimal projects
 		ArrayList<project> best = new ArrayList<project>();
 		int x = numweeks-1;		//start at the end of the array
 		int y = numproj-1;
@@ -89,19 +94,18 @@ public class scheduler {
 		while (filledTable[x][y] != 0){
 			if (y==0){
 				x--;
-				y = numproj - 1;
-				
+				y = numproj - 1;	
 			}
 			else if (filledTable[x][y] != filledTable[x][y-1]){
 				best.add(projList.get(y));
-				if (projList.get(y).start==1)
+				if (projList.get(y).start==1) //if you find the starting project
 					break;
-				x = projList.get(y).start - 2; //goes to index containing week before start
+				x = projList.get(y).start - 2; //goes to index of the week before this project's start
 				y = numproj - 1;
 			}
 			else
 				y--;
-			
+
 		}
 		//generate message listing projects
 		String msg = "";
@@ -111,7 +115,7 @@ public class scheduler {
 				" and will do the following projects:\n" + msg;
 		return msg;
 	}
-	
+
 	/**
 	 * returns max of ints a & b
 	 * @return
@@ -123,14 +127,13 @@ public class scheduler {
 
 	private class project{
 		public int num, start, val, end;
-		
 		project(int projectnumber, int starttime, int duration, int value){
 			num = projectnumber;
 			start = starttime;
 			val = value;
 			end = start + duration - 1;
 		}
-		
+
 		public String printMe(){
 			return "Project #"+num+" starts at beginning of week "+start+ 
 					" and lasts until week "+end;
@@ -142,9 +145,9 @@ public class scheduler {
 	public void printMe() {
 		System.out.println("Printing all projects for file "+file);
 		System.out.println("Contains "+numproj+ " projects over "+numweeks+" weeks");
-		for (project p :projList){
-			System.out.println("Proj #"+p.num+" lasts from start of week "+p.start+" until end of week "+p.end+" and is worth "+p.val);
-		}
+		for (project p :projList)
+			System.out.println("Proj #"+p.num+" lasts from start of week "+p.start+
+					" until end of week "+p.end+" and is worth "+p.val);
 
 	}
 }
